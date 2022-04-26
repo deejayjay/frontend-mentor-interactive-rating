f56b4fc2ca2d500480ec373ef0b29661aaf10556
Deepak Joy Jose <deejayjay@zoho.com>
2022-04-26 13:56:24 -0600

    Adds the JavaScript to make the rating functionality working.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-console.log("Hello from JavaScript");
+const ratingForm = document.forms["ratingForm"];
+const radioBtns = ratingForm.querySelectorAll(".rating-radio-btn > input");
+
+ratingForm.addEventListener("submit", handleRatingSubmit);
+
+function handleRatingSubmit(e) {
+  e.preventDefault();
+
+  const checkedRadio = e.target.querySelector(
+    ".rating-radio-btn > input:checked"
+  );
+  const rating = checkedRadio.value.trim();
+
+  document.querySelector(
+    ".rating-result"
+  ).textContent = `You selected ${rating} out of 5`;
+
+  document.querySelector(".rating-form-wrapper").classList.add("hidden");
+  document.querySelector(".thank-you-wrapper").classList.remove("hidden");
+}
