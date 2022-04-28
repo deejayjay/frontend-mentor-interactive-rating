31a3be2d0a6e7b559c1e0394cd934d0583d8a147
Deepak Joy Jose <deejayjay@zoho.com>
2022-04-28 11:21:35 -0600

    Adds desktop styles and JavaScript to show toast message if no rating is selected

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,42 @@
 const ratingForm = document.forms["ratingForm"];
 const radioBtns = ratingForm.querySelectorAll(".rating-radio-btn > input");
+const warning = document.querySelector(".rating-warning");
 
 ratingForm.addEventListener("submit", handleRatingSubmit);
 
+// Handles the rating form submit event
 function handleRatingSubmit(e) {
   e.preventDefault();
 
   const checkedRadio = e.target.querySelector(
     ".rating-radio-btn > input:checked"
   );
-  const rating = checkedRadio.value.trim();
+  const rating = checkedRadio?.value.trim();
 
-  document.querySelector(
-    ".rating-result"
-  ).textContent = `You selected ${rating} out of 5`;
+  if (rating) {
+    document.querySelector(
+      ".rating-result"
+    ).textContent = `You selected ${rating} out of 5`;
 
-  document.querySelector(".rating-form-wrapper").classList.add("hidden");
-  document.querySelector(".thank-you-wrapper").classList.remove("hidden");
+    document.querySelector(".rating-form-wrapper").classList.add("hidden");
+    document.querySelector(".thank-you-wrapper").classList.remove("hidden");
+  } else {
+    warning.classList.remove("hidden");
+    warning.addEventListener("click", handleWarningClose);
+
+    // Trigger the click event of warning after 3 seconds
+    setTimeout(() => {
+      warning.click();
+    }, 3000);
+  }
+}
+
+// Handles the warning popup click event
+function handleWarningClose(e) {
+  const warning = e.target;
+
+  document
+    .querySelector(".rating-warning")
+    .removeEventListener("click", handleWarningClose);
+  warning.classList.add("hidden");
 }
